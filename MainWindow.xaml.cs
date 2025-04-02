c54df7b06b428ec62efdccf1cf53e87b232c563c
KrzysztofLata <133794261+KrzysztofLata@users.noreply.github.com>
2025-04-02 13:02:25 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    class item
+    {   
+        public int id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string pesel { get; set; }
+        public item(int i, string n, string s,string p) {
+            id = i;
+            name = n;
+            surname = s;
+            pesel = p;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 newWindow = new Window1();
+            newWindow.ShowDialog();
+            item x = new item(listView.Items.Count+1, newWindow.xname, newWindow.xsurname, newWindow.xpes);
+            listView.Items.Add(x);
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public string xpes;
+        public string xname;
+        public string xsurname;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            xpes = pesel.Text;
+            xname = name.Text;
+            xsurname = surname.Text;
+            Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult res = MessageBox.Show("Czy chcesz zamknąć?",, MessageBoxButton.YesNo);
+            if (res==MessageBoxResult.Yes)
+            {
+                Hide();
+            }
+        }
+    }
+}
