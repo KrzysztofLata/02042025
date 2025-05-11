ebab012d5b939ac41de143e71b925fb66d67315c
KrzysztofLata <133794261+KrzysztofLata@users.noreply.github.com>
2025-05-11 18:07:47 +0200

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using Microsoft.Win32;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,7 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Collections.ObjectModel;
 namespace WpfApp1
 {
     /// <summary>
@@ -21,13 +22,17 @@ namespace WpfApp1
         public string name { get; set; }
         public string surname { get; set; }
         public string pesel { get; set; }
-        public string name2nd {get;set;}
+        public string name2nd { get; set; }
         public string adres { get; set; }
         public string city { get; set; }
         public string date { get; set; }
         public string phone { get; set; }
         public string code { get; set; }
-        public item(int i, string n, string s,string p, string n2,string a,string c,string d,string ph, string co) 
+        public item()
+        {
+
+        }
+        public item(int i, string n, string s, string p, string n2, string a, string c, string d, string ph, string co)
         {
             id = i;
             name = n;
@@ -57,24 +62,16 @@ namespace WpfApp1
             {
                 char ddd = d[i];
                 string dddd = ddd.ToString();
-                if (int.TryParse(dddd, out _) == false && d[i] != '-' && d[i] != '/')
+                if (int.TryParse(dddd, out _) == false && d[i] != '.')
                 {
                     return false;
                 }
             }
-            int l1 = d.IndexOf('-');
-            int l2 = d.LastIndexOf('-');
-            if (l1 == -1)
-            {
-                l1 = d.IndexOf('/');
-            }
-            if (l2 == -1)
-            {
-                l2 = d.LastIndexOf('/');
-            }
-            int rok = (int.Parse(d.Substring(0, l1)));
-            int miesiac = (int.Parse(d.Substring(l1 + 1, l2)));
-            int dzien = (int.Parse(d.Substring(l2 + 1)));
+            int l1 = d.IndexOf('.');
+            int l2 = d.LastIndexOf('.');
+            int dzien = (int.Parse(d.Substring(0, 2)));
+            int miesiac = (int.Parse(d.Substring(l1 + 1, 2)));
+            int rok = (int.Parse(d.Substring(l2 + 1)));
             if (rok < 1800 || rok > 2299)
             {
                 return false;
@@ -90,20 +87,21 @@ namespace WpfApp1
             if (pes != "0")
             {
                 pes = pes.Substring(0, 6);
-                int pesD = int.Parse(pes.Substring(0, 2));
+                int pesD = int.Parse(pes.Substring(4, 2));
                 if (pesD != dzien)
                 {
                     return false;
                 }
                 string rk = rok.ToString();
-                if (rk.Substring(2) != pes.Substring(4))
+                if (rk.Substring(2) != pes.Substring(0, 2))
                 {
                     return false;
                 }
-                string w = pes.Substring(2, 3);
+                string w = pes.Substring(2, 1);
+                Console.WriteLine(rk[1]);
                 if (miesiac < 10)
                 {
-                    if ((w == "8" && rk[2] == '8') || (w == "0" && rk[2] == '9') || (w == "4" && rk[2] == '1') || (w == "6" && rk[2] == '2'))
+                    if ((w == "8" && rk[1] == '8') || (w == "0" && rk[1] == '9') || (w == "4" && rk[1] == '1') || (w == "2" && rk[1] == '0'))
                     {
                         return true;
                     }
@@ -117,6 +115,16 @@ namespace WpfApp1
 
             return true;
         }
+        char fplec(string name)
+        {
+            char[] y = name.ToCharArray();
+            int x = y.Length - 1;
+            if (y[x] == 'a')
+            {
+                return 'K';
+            }
+            return 'M';
+        }
         bool fpesel(string p, string d, char m)
         {
             if (p.Length > 11)
@@ -127,22 +135,30 @@ namespace WpfApp1
             int[] waga = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
             for (int i = 0; i < 10; i++)
             {
-                tablica[i] = int.Parse(p.Substring(i, i + 1));
+                tablica[i] = int.Parse(p.Substring(i, 1));
                 tablica[i] *= waga[i];
+                if (tablica[i] > 9)
+                {
+                    tablica[i] = tablica[i] % 10;
+                }
+
             }
             int sum = tablica.Sum();
             sum %= 10;
             sum = 10 - sum;
-            sum %= 10;
-            if (sum != p[10])
+            if (sum == 10)
+            {
+                sum = 0;
+            }
+            if (sum != Int32.Parse(p[10].ToString()))
             {
                 return false;
             }
-            if (int.Parse(p.Substring(9, 10)) % 2 == 0 && (m == 'M'))
+            if (int.Parse(p.Substring(9, 1)) % 2 == 0 && (m == 'M'))
             {
                 return false;
             }
-            if (int.Parse(p.Substring(9, 10)) % 2 == 1 && (m == 'K'))
+            if (int.Parse(p.Substring(9, 1)) % 2 == 0 && (m == 'K'))
             {
                 return false;
             }
@@ -151,16 +167,6 @@ namespace WpfApp1
                 return false;
             }
             return true;
-        }
-        public char fplec(string name)
-        {
-            int x = name.Length - 1;
-            char [] y = name.ToCharArray();
-            if (y[x]=='a')
-            {
-                return 'K';
-            }
-                return 'M';
         }
         string fnazwisko(string n)
         {
@@ -179,13 +185,13 @@ namespace WpfApp1
                 xzzz = xzzz.ToUpper();
                 n = n.Substring(0, xxx + 1) + xzzz + n.Substring(xxx + 2);
             }
-            
+
             return n;
         }
         string fphone(string num)
         {
             int x = num.IndexOf("+");
-            if(x==-1)
+            if (x == -1 && num.Length == 9)
             {
                 num = "+48" + num;
             }
@@ -196,18 +202,99 @@ namespace WpfApp1
         {
             Window1 newWindow = new Window1();
             newWindow.ShowDialog();
-            //if (fpesel(newWindow.xpes, newWindow.date_of_birth.ToString(),fplec(newWindow.xname)) == true)
+            if (fpesel(newWindow.xpes, newWindow.xdate, fplec(newWindow.xname)) == true)
             {
                 newWindow.xsurname = fnazwisko(newWindow.xsurname);
                 newWindow.xname = fnazwisko(newWindow.xname);
-                newWindow.xname2nd = fnazwisko(newWindow.xname2nd);
+                if (newWindow.xname2nd.Length > 0) newWindow.xname2nd = fnazwisko(newWindow.xname2nd);
                 newWindow.xcity = fnazwisko(newWindow.xcity);
                 newWindow.xadres = fnazwisko(newWindow.xadres);
                 newWindow.xphone = fphone(newWindow.xphone);
                 item x = new item(listView.Items.Count + 1, newWindow.xname, newWindow.xsurname, newWindow.xpes, newWindow.xname2nd, newWindow.xadres, newWindow.xcity, newWindow.xdate, newWindow.xphone, newWindow.xcode);
                 listView.Items.Add(x);
             }
-                
+
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            while (listView.SelectedItems.Count > 0)
+            {
+                listView.Items.Remove(listView.SelectedItems[0]);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            saveFileDialog.Title = "Zapisz jako plik CSV";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string delimiter = ";";
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    foreach (item item in listView.Items)
+                    {
+                        var row = $"{item.id}{delimiter}{item.name}{delimiter}{item.name2nd}{delimiter}"+
+                            $"{item.surname}{delimiter}{item.date}{delimiter}{item.pesel}{delimiter}"+
+                            $"{item.code}{delimiter}{item.city}{delimiter}{item.adres}{delimiter}{item.phone}";
+                        writer.WriteLine(row);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            ///funkcja menu „open”:
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Pliki CSV z separatorem (,) |*.csv|Pliki CSV z separatorem (;) |*.csv";
+            openFileDialog.Title = "Otwórz plik CSV";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                listView.Items.Clear();
+                string filePath = openFileDialog.FileName;
+                int selectedFilterIndex = openFileDialog.FilterIndex;
+                string delimiter = ";";
+                if (selectedFilterIndex == 1)
+                {
+                    delimiter = ",";
+                }
+                Encoding encoding = Encoding.UTF8;
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath, encoding);
+                    foreach (var line in lines)
+                    {
+                        string[] columns = line.Split(delimiter);
+                        if (columns != null)
+                        {
+                            item uczen = new item();
+                            uczen.pesel = columns.ElementAtOrDefault(5);
+                            uczen.name = columns.ElementAtOrDefault(1);
+                            uczen.name2nd = columns.ElementAtOrDefault(2);
+                            uczen.surname = columns.ElementAtOrDefault(3);
+                            uczen.date = columns.ElementAtOrDefault(4);
+                            uczen.code = columns.ElementAtOrDefault(6);
+                            uczen.city = columns.ElementAtOrDefault(7);
+                            uczen.adres = columns.ElementAtOrDefault(8);
+                            uczen.phone = columns.ElementAtOrDefault(9);
+                            int id = Int32.Parse(columns.ElementAtOrDefault(0));
+                            uczen.id =id;
+                            listView.Items.Add(uczen);
+
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
